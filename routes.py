6fd892bf9f8e28dd6a9973612666fe1edfd07a8c
Amy Paull <16284@burnside.school.nz>
2020-05-21 20:38:14 +1200

    started attempt at search form intergration. didn't work well so left it

--- routes.py
+++ routes.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-#from config import Config
+from config import Config
 import sqlite3
+from forms import SearchForm
 
 app=Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///POW_Project.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'ajaj'
 
 db = SQLAlchemy(app)
 
@@ -20,9 +22,9 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route('/search')
+@app.route('/records')
 def search():
-    return render_template("search.html")
+    return render_template("records.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
