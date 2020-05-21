aa7f0da430cf82b4d6fc7880f4cbaf6e5308a343
Amy Paull <16284@burnside.school.nz>
2020-05-21 16:38:25 +1200

    created about page and search page

--- routes.py
+++ routes.py
@@ -16,5 +16,13 @@ import models
 def home():
     return render_template("home.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/search')
+def search():
+    return render_template("search.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
